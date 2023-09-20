dbcf11b80e13f16cd71f2db71c69f1679fc56dd1
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-20 17:53:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import Map from './components/Map'
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async() =>{
+      setLoading(true)
+      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+      const {events} = await res.json
+
+      setEventData(events)
+      setLoading(false)
+
+    }
+    fetchEvents()
+
+    console.log(eventData)
+  }, [])
+
+
   return (
     <div>
-      <h1>Welcome to the App</h1>
+      <Map/>
     </div>
   );
 }
